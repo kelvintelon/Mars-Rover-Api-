60c104dbd7d00740a6b63990e390f494d6385b6d
Kelvin Telon <telon.kelvin77@gmail.com>
2022-03-21 16:35:19 -0400

    I preferred Thyme because its dynamic, gets the value from the Model's attribute. Begun to disect the response body from a simple get but it's not returning any data so will need to debug

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String getHome() {
+	public String getHome(ModelMap model) {
+		
 		return "index";
 	}
 }
